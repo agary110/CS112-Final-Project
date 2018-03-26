28c58508c10601b270aed82f564a265f425ca44b
Sylvia Frank 20 <sfrank20@romulus.amherst.edu>
2018-03-26 12:36:41 -0400

    test

--- FinalProj.java
+++ FinalProj.java
@@ -3,5 +3,6 @@ public class FinalProj{
 	public static void main(String args[]){
 		System.out.println("hello world");
 		System.out.println("Where is Amy?");
+		System.out.println("haha i'm bored.");
 	}
 }
