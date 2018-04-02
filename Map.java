9bd8db0181c8487b3872cb99c9bed153cfccf626
Amy Pass <amypass@vlan417-44.subnet-232.amherst.edu>
2018-04-02 12:37:03 -0400

    Map

---
+++ Map.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.LinkedList;
+
+class Map{
+	public static List<Path> upcomingPaths;
+	public static Marble marble;
+	public double speed;
+
+	public Map(){
+		
+	}
+}
