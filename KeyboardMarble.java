2a1ff78cfc6b1a41f5f45996218154c35db90417
Annabelle Gary <annabellegary@vlan432-7.subnet-231.amherst.edu>
2018-04-03 22:16:43 -0400

    commit

---
+++ KeyboardMarble.java
@@ -0,0 +1,156 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.util.Random;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+class Pair{
+    public double x;
+    public double y;
+    
+    public Pair(double initX, double initY){
+	x = initX;
+	y = initY;
+    }
+
+    public Pair add(Pair toAdd){
+	return new Pair(x + toAdd.x, y + toAdd.y);
+    }
+
+    public Pair divide(double denom){
+	return new Pair(x / denom, y / denom);
+    }
+
+    public Pair times(double val){
+	return new Pair(x * val, y * val);
+    }
+
+    public void flipX(){
+	x = -x;
+    }
+    
+    public void flipY(){
+	y = -y;
+    }
+}
+
+
+class Marble{
+    Pair position;
+    Pair velocity;
+    Pair acceleration;
+    double radius;
+    double dampening;
+    Color color;
+    public Sphere()
+    {
+	Random rand = new Random(); 
+	position = new Pair(500.0, 500.0);
+	velocity = new Pair((double)(rand.nextInt(1000) - 500), (double)(rand.nextInt(1000) - 500));
+	acceleration = new Pair(0.0, 0.0);
+	radius = 25;
+	dampening = 1.3;
+	color = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
+    }
+    public void update(World w, double time){
+	position = position.add(velocity.times(time));
+	velocity = velocity.add(acceleration.times(time));
+	bounce(w);
+    }
+    
+    public void setPosition(Pair p){
+	position = p;
+    }
+    public void setVelocity(Pair v){
+	velocity = v;
+    }
+    public void setAcceleration(Pair a){
+	acceleration = a;
+    } 
+    public void draw(Graphics g){
+	Color c = g.getColor();
+	
+	g.setColor(color);
+	g.fillOval((int)(position.x - radius), (int)(position.y - radius), (int)(2*radius), (int)(2*radius));
+	g.setColor(c);
+    }
+
+}
+
+public class KeyboardSpheres extends JPanel implements KeyListener{
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = 768;
+    public static final int FPS = 60;
+    World world;
+
+    class Runner implements Runnable{
+	public void run()
+	{
+	    while(true){
+		world.updateSpheres(1.0 / (double)FPS);
+		repaint();
+		try{
+		    Thread.sleep(1000/FPS);
+		}
+		catch(InterruptedException e){}
+	    }
+
+	}
+    
+    }
+
+    public void keyPressed(KeyEvent e) {
+        char c=e.getKeyChar();
+	System.out.println("You pressed down: " + c);			
+
+    }
+    public void keyReleased(KeyEvent e) {
+        char c=e.getKeyChar();
+	System.out.println("\tYou let go of: " + c);
+	
+    }
+
+    public void keyTyped(KeyEvent e) {
+	char c = e.getKeyChar();
+	System.out.println("You typed: " + c);
+	
+    }
+     public void addNotify() {
+        super.addNotify();
+        requestFocus();
+    }
+
+    public KeyboardSpheres(){
+	world = new World(WIDTH, HEIGHT, 50);
+	addKeyListener(this);
+	this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	Thread mainThread = new Thread(new Runner());
+	mainThread.start();
+    }
+    
+    public static void main(String[] args){
+	JFrame frame = new JFrame("Physics!!!");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	KeyboardSpheres mainInstance = new KeyboardSpheres();
+	frame.setContentPane(mainInstance);
+	frame.pack();
+	frame.setVisible(true);
+    }
+
+
+    public void paintComponent(Graphics g) {
+	super.paintComponent(g);    	
+
+	g.setColor(Color.BLACK);
+	g.fillRect(0, 0, WIDTH, HEIGHT);
+
+	world.drawSpheres(g);
+
+    }
+
+    
+}
+
--- World.java
+++ World.java
@@ -1,10 +1,17 @@
 public class World{
     int height;
     int width;
+    Marble marble;
 
     public World(int initHeight, int initwidth){
 	height = initheight;
 	width = initwidth;
    
     }
+	
+    public void drawMarble(Graphics g){
+	g.fillOval(
+    }
+
+
 }
