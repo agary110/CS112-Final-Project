8056c2b3ea204c8d96fa7feb3cefaf4bc9becb50
sylviafrank8 <37809163+sylviafrank8@users.noreply.github.com>
2018-04-06 11:33:31 -0400

    Update LeftCorner.java
    Deleted unnecessary imports

--- LeftCorner.java
+++ LeftCorner.java
@@ -3,10 +3,8 @@ import java.awt.Graphics;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
-import java.util.Random;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.Ellipse2D;
 import java.awt.RenderingHints;
 
 public class LeftCorner extends Path {
