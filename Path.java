508def651b655460a73618b806fb0a63609ab956
sylviafrank8 <37809163+sylviafrank8@users.noreply.github.com>
2018-04-06 11:16:13 -0400

    Create Path.java

---
+++ Path.java
@@ -0,0 +1,28 @@
+public class Path {
+    boolean bumpersOn=false;
+    final int bumperWidth; //need to decide what this is
+    final int WIDTH;
+    final int HEIGHT;
+    int x; //corresponds to path's upper left corner
+    int y; //corresponds to path's upper left corner
+    //  final int gapLength; //are we still doing this?
+    String name;
+
+    public Path() {
+	name = "";
+	bumperWidth=10; //random for now
+	WIDTH=200;
+	HEIGHT=300;
+	x=800;
+	y=450;
+    }
+
+    public void changeX(){
+	//not sure what we're putting here?
+    }
+
+    public void changeY(){
+
+	//same as changeX
+    }
+}
