69de9d08e34614ebc5762b22a0a03db05ceb1862
sylviafrank8 <37809163+sylviafrank8@users.noreply.github.com>
2018-04-13 20:54:10 -0400

    Update LeftCorner.java

--- LeftCorner.java
+++ LeftCorner.java
@@ -3,22 +3,25 @@ import java.awt.Graphics;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
+import java.util.Random;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.RenderingHints;
 
 public class LeftCorner extends Path {
 
-    public LeftCorner(int x) {
-	super(x);
-	super.name="leftCorner";
+    public LeftCorner(int exitX) {
+	super(exitX);
+	this..name="leftCorner";
+	this.x=exitX-super.WIDTH;
     }
-    public void drawLeftCorner(Graphics g0ri){
+    public void draw(Graphics g0ri){
 	Graphics2D g = (Graphics2D) g0ri;
 	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 			   RenderingHints.VALUE_ANTIALIAS_ON);
  
 	g.fill (new Rectangle2D.Double(super.x, super.y, 2*super.WIDTH, super.HEIGHT/2));
-	g.fill (new Rectangle2D.Double(2*super.x, 2*super.y, super.WIDTH, super.HEIGHT/2));
+	g.fill (new Rectangle2D.Double(super.x+super.WIDTH/2, super.y+super.HEIGHT/2, super.WIDTH, super.HEIGHT/2));
     }
 }
