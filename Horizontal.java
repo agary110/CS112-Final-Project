5b7df939e6beb5af7797fc5d44bba32a811a38a4
sylviafrank8 <37809163+sylviafrank8@users.noreply.github.com>
2018-04-13 20:56:03 -0400

    Create Horizontal.java

---
+++ Horizontal.java
@@ -0,0 +1,32 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.RenderingHints;
+
+
+public class Horizontal extends Path{
+    boolean direction;
+
+    public Horizontal(int exitX, boolean direction){
+	super(exitX);
+	this.name="Horizontal";
+	if (direction) {
+	    this.x=exitX+2*WIDTH;
+	}
+	else {
+	    this.x=exitX-2*WIDTH;
+	}
+    }
+
+    public void draw(Graphics g0ri){
+	Graphics2D g = (Graphics2D) g0ri;
+	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON);
+	g.fill(new Rectangle2D.Double(super.x, super.y, super.HEIGHT, 2*super.WIDTH));
+    }
+}
