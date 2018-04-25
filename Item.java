b85d29295f4e8e26c5b000d1d9c911ac223e5a95
Annabelle Gary <annabellegary@vlan432-43.subnet-231.amherst.edu>
2018-04-24 21:51:31 -0400

    trying to work on bumpers

--- Item.java
+++ Item.java
@@ -379,6 +379,12 @@ class Bumpers extends Booster{
 		//How to actually turn the bumpers off
 		
 	}
+
+	public void update(){
+		super.update();
+		World.marble.canMove = true;
+		Game.alive = true;
+	}
 	//NEED: a draw method which will add some color to the edge of the path. It can do this by going through all the paths 
 }
 
