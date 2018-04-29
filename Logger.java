213f2c2ec56008da81f0994ba16bca3795b3e6cf
Annabelle Gary <annabellegary@vlan432-106.subnet-231.amherst.edu>
2018-04-29 13:22:17 -0400

    added logging class, still need to incorporate it into game class

---
+++ Logger.java
@@ -0,0 +1,49 @@
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
+
+public class Logger{
+	public static void writeHighScore(double points, String outFileName){
+		try {
+			double currentHighScore = readHighScore(outFileName);
+			if (points >= currentHighScore){
+				File output = new File(outFileName);
+				PrintWriter pointPrinter = new PrintWriter(output);
+				pointPrinter.write("" + points);
+				pointPrinter.close();
+			} else {
+				File output = new File(outFileName);
+				PrintWriter pointPrinter = new PrintWriter(output);
+				pointPrinter.write("" + currentHighScore);
+				pointPrinter.close();
+			}
+		}
+		catch(FileNotFoundException e) {
+	   		System.err.println("File not found.");
+	   		System.err.println(e);
+		}
+	}
+
+	public static double readHighScore(String inFileName){
+		try {
+			File input = new File(inFileName);
+			Scanner sc = new Scanner(input);
+			while(sc.hasNextDouble()){
+				return sc.nextDouble();
+			}
+		}
+		catch(FileNotFoundException e) {
+	   		System.err.println("File not found.");
+	   		System.err.println(e);
+		}
+		return 0.0;
+	}
+
+	public static void main (String args[]){
+		writeHighScore(3, "highscore.txt");
+	//	double a = readHighScore("highscore.txt");
+		//System.out.println(a);
+	}
+
+}
