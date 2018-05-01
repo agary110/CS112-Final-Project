46c6d31063c4934b3feb47b473a3039b97411555
Amy Pass <amypass@vlan417-40.subnet-232.amherst.edu>
2018-05-01 11:03:18 -0400

    fixed beginning map problem

--- Map.java
+++ Map.java
@@ -26,7 +26,7 @@ class Map{
 		allMaps = new LinkedList<LinkedList<Path>>();
 
 		upcomingPaths.add(new Straight(Game.WIDTH / 2 - Path.WIDTH / 2));
-		upcomingPaths.add(new Trapezoid(Game.HEIGHT));
+		upcomingPaths.add(new Trapezoid(Game.HEIGHT / 2));
 		upcomingPaths.add(new Straight(upcomingPaths.getLast()));
 		upcomingPaths.get(0).y = Game.HEIGHT;
 		for(int i = 1; i < 6; i++){
--- Trapezoid.java
+++ Trapezoid.java
@@ -3,8 +3,7 @@ import java.awt.Color;
 
 public class Trapezoid extends Path{
 	static final int HEIGHT = Game.HEIGHT / 2;
-	int [] [] triangle1;
-	int [] [] triangle2;
+	int [] [] coordinates;
 
 	public Trapezoid(int y){
 		super(0);
@@ -12,7 +11,17 @@ public class Trapezoid extends Path{
 		x = Game.WIDTH / 2 - Path.WIDTH / 2;
 		this.y = y;
 
-		triangle1 = new int [2][3];
+		coordinates = new int [2] [4];
+		coordinates [0][0] = exitX;
+		coordinates [0][1] = Game.WIDTH / 2 - Path.WIDTH / 2;
+		coordinates [0][2] = Game.WIDTH / 2 + Path.WIDTH / 2;
+		coordinates [0][3] = Game.WIDTH;
+		coordinates [1][0] = Game.HEIGHT;
+		coordinates [1][1] = y;
+		coordinates [1][2] = y;
+		coordinates [1][3] = Game.HEIGHT;
+
+		/*triangle1 = new int [2][3];
 		triangle1 [0][0] = 0;
 		triangle1 [0][1] = x;
 		triangle1 [0][2] = x;
@@ -26,22 +35,22 @@ public class Trapezoid extends Path{
 		triangle2 [0][2] = triangle1 [0][1] + Path.WIDTH;
 		triangle2 [1][0] = y + HEIGHT;
 		triangle2 [1][1] = y + HEIGHT;
-		triangle2 [1][2] = y;
+		triangle2 [1][2] = y;*/
 		
 	}
 
 	public void draw(Graphics g){
 		g.setColor(color);
-		g.fillPolygon(triangle1[0], triangle1[1], triangle1[0].length);
-		g.fillPolygon(triangle2[0], triangle2[1], triangle2[0].length);
-		g.fillRect(triangle1[0][2], triangle1[1][2], Path.WIDTH, Game.HEIGHT / 2);
+		g.fillPolygon(coordinates[0], coordinates[1], coordinates[0].length);
+		/*g.fillPolygon(triangle2[0], triangle2[1], triangle2[0].length);
+		g.fillRect(triangle1[0][2], triangle1[1][2], Path.WIDTH, Game.HEIGHT / 2);*/
 	}
 
 	public void update(){
-		for(int i = 0; i < triangle1[1].length; i++){
-			System.out.println("updating");
-			triangle1[1][i]++;
-			triangle2[1][i]++;
+		for(int i = 0; i < coordinates[1].length; i++){
+			coordinates[1][i]++;
+System.out.println("trap y: " + coordinates[1][1]);
+			//triangle2[1][i]++;
 		}
 	}
 }
