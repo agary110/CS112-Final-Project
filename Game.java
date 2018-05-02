6dbe5785463b647e9ac647342c85adaeb7168546
Annabelle Gary <annabellegary@vlan432-230.subnet-231.amherst.edu>
2018-05-02 10:16:43 -0400

    cleaned up Game class

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
-//================================================
+//================================
 /** Import necessary libraries **/
+//================================
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import java.awt.event.KeyListener;
@@ -8,10 +9,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
 
+
+//==============================================
+/** The Game class is the main class, which the user Runs. It makes a JPanel pop  up, where the game is played, and it takes in user key inputs. **/
+
 public class Game extends JPanel implements KeyListener{
 
-//================================================
+//======================
 /** Member variables **/
+//======================
 	public static final int WIDTH = 1400;
 	public static final int HEIGHT = 750;
 	public static int FPS = 90;
@@ -23,15 +29,15 @@ public class Game extends JPanel implements KeyListener{
 	public static boolean kpressed;
 	public static boolean lpressed;
 	public static boolean pressed;
-
-
 	public static boolean hasGameStarted;
 	public static boolean paused;
 	char c;
-
 	public static double currentHighScore;
 
 
+//======================
+/**   Constructor    **/
+//======================
 	public Game(){
 		world = new World();
 		this.addKeyListener(this); 
@@ -49,8 +55,9 @@ public class Game extends JPanel implements KeyListener{
 		currentHighScore = Logger.readHighScore("highscore.txt");
 	}
 
-//================================================
-/** Run method; **/
+//======================
+/** Internal runner  **/
+//======================
 	class Runner implements Runnable{
 		public void run(){
 
@@ -75,6 +82,14 @@ public class Game extends JPanel implements KeyListener{
 		}
 	}
 
+//======================
+/**     Methods      **/
+//======================
+
+//==================================================================
+/** keyPressed(KeyEvent e)
+	Functionality: takes user input of keys and uses them to perform functions. **/
+//==================================================================
 	public void keyPressed(KeyEvent e) {
       	c = e.getKeyChar();
 		pressed=true;
@@ -88,6 +103,11 @@ public class Game extends JPanel implements KeyListener{
 		}
 
     }
+
+//==================================================================
+/** keyTyped(KeyEvent e)
+	Functionality: Same as keyPressed, but with different keys. Takes user input of keys and uses them to perform functions. **/
+//==================================================================
 
 	public void keyTyped(KeyEvent e) {
 		c = e.getKeyChar();
@@ -116,6 +136,11 @@ public class Game extends JPanel implements KeyListener{
 			}
 		}
 	}
+//==================================================================
+/** keyReleased(KeyEvent e)
+	Functionality: performs functions when a user releases a key. **/
+//==================================================================
+
 
 	public void keyReleased(KeyEvent e) {
 		c=e.getKeyChar();
@@ -133,26 +158,28 @@ public class Game extends JPanel implements KeyListener{
 		}
     }
 
-	public static void main(String [] args){
-		JFrame frame = new JFrame("aMAZE-ing Maze");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Game mainInstance = new Game();
-		frame.setContentPane(mainInstance);
-		frame.pack();
-		frame.setVisible(true);
-	}
-
+//============================================================
+/** Graphics paint method:
+    Functionality: paintComponent gets called as repaint() in the run() method. It calls the draw methods for everything in the Game every frame. **/
+//============================================================
 	public void paintComponent(Graphics g){
+		// background
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		// Graphics if game has started
 		if (hasGameStarted) {
 			world.drawToScreen(g);
 		}
+
+		// Graphics if game hasn't started
 		else{
 			g.setColor(Color.GREEN);
 			g.drawString("Hello! To begin playing, press the space bar.", WIDTH / 3, HEIGHT / 2);
 		}
+
+		// Graphics when game is over:
 		if(!alive){
 			g.setColor(Color.WHITE);
 			g.fillRect(Game.WIDTH / 2 - (Game.WIDTH/4)/2, Game.HEIGHT / 2 - (Game.HEIGHT/4)/2, Game.WIDTH / 4, Game.HEIGHT / 4);
@@ -172,8 +199,25 @@ public class Game extends JPanel implements KeyListener{
 				g.drawString("You did not beat the high score. Try again!", Game.WIDTH / 4 + 200, Game.HEIGHT / 4 + 190);
 				g.drawString("Current high score: " + (int)currentHighScore, Game.WIDTH/2 - 70, Game.HEIGHT/4+220);
 			}
-
 		}
 	}
 
+
+//======================
+/**    Main method   **/
+//======================
+	public static void main(String [] args){
+		JFrame frame = new JFrame("aMAZE-ing Maze");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Game mainInstance = new Game();
+		frame.setContentPane(mainInstance);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+
 }
+
+
+/** End of Game class **/
+//==============================================
